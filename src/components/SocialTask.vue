bad0ef232fcd2c2c8275ff2ec57466dbfde2e06e
thejackforge <kimfidler@gmail.com>
2021-06-05 08:23:17 -0600

    Added Delete Complete Function

---
+++ src/components/SocialTask.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="task">
+        <div class="task-title-icon-container" :class="{ complete: socialTask.complete }">
+            <div class="task-title">
+            <h3>{{ socialTask.taskTitle}} ({{ socialTask.socialNetwork }})</h3>
+            <button class="details-button" @click="showDetails" ><span v-if="this.showDetail">HIDE</span><span v-if="!this.showDetail">SHOW</span></button>
+            </div>
+            <div class="action-icons">
+                <div class="icon-container"><span class="material-icons">
+edit
+</span></div>
+                <div class="icon-container" @click="deleteTask"><span class="material-icons">
+delete
+</span></div>
+                <div class="icon-container" @click="completeTask"><span class="material-icons mark-complete">
+done_outline
+</span></div>
+            </div>
+        </div>
+        
+            <div class="task-details" v-show="showDetail">
+                <p>{{ socialTask.details }}</p>
+            </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'socialTask' ],
+    data() {
+        return {
+            showDetail: false,
+            endPoint: 'http://localhost:3000/socialTasks/' + this.socialTask.id
+        }
+    },
+    methods: {
+        showDetails() {
+            this.showDetail = !this.showDetail
+        },
+        deleteTask() {
+            console.log('DELETE TASK')
+            fetch(this.endPoint, { method: 'DELETE' })
+            .then(() => this.$emit( 'removeTask', this.socialTask.id ))
+            .catch((err) => console.log(err.message))
+        },
+        completeTask() {
+            console.log('COMPLETE TASK')
+            fetch(this.endPoint, { method: 'PATCH' ,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({complete: !this.socialTask.complete})
+            })
+            .then(() => this.$emit ('completeTask', this.socialTask.id))
+            .catch((err) => console.log(err.message))
+        }
+    }
+}
+</script>
+
+<style>
+
+.task {
+    width: 600px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    background: rgb(243, 243, 243);
+    margin-bottom: 20px;
+    border-radius: 10px;
+    padding: 20px 0 20px 20px;
+}
+
+.task-title-icon-container {
+    display: flex;
+    justify-content: space-between;
+    background: rgb(243, 243, 243);
+}
+
+.action-icons {
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+}
+
+.icon-container {
+    margin: 0 5px;
+}
+
+.details-button {
+    border: none;
+    background-color: rgb(200, 235, 177);
+    padding: 10px 50px;
+    border-radius: 8px;
+    cursor: pointer;
+}
+
+.details-button:active {
+    transform: scale(0.99);
+}
+
+.material-icons {
+    cursor: pointer;
+}
+
+.task-title-icon-container.complete .mark-complete {
+    color: rgb(94, 255, 0);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-main-container">
+    <div class="social-task-main-container" v-if="socialTasks.length">
+      <div v-for="socialTask in socialTasks" :key="socialTask.id">
+        <SocialTask :socialTask="socialTask" @removeTask="removeTask" @completeTask="completeTask"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import SocialTask from '../components/SocialTask.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { SocialTask },
+  data() {
+    return {
+      socialTasks: [],
+      endPoint: 'http://localhost:3000/socialTasks/'
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/socialTasks')
+    .then( res => res.json())
+    .then( data => this.socialTasks = data)
+    .catch( err => console.log(err.message))
+  },
+  methods: {
+    removeTask(id) {
+      this.socialTasks = this.socialTasks.filter(( task => {
+        return task.id !== id
+      }))
+    },
+    completeTask(id) {
+      console.log(id)
+      let t = this.socialTasks.find(task => {
+        return task.id === id
+      })
+      t.complete = !t.complete
+    }
   }
 }
 </script>
+
+<style scoped>
+
+.home-main-container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.social-task-main-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+</style>
